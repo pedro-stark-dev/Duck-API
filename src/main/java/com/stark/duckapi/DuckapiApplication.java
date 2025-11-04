6467c9670dbeb980027369691c6cc8c69acf7078
pedro-stark-dev <joaopedrocaruaru2020.2022@gmail.com>
2025-11-04 15:23:17 -0300

    my first commit

---
+++ src/main/java/com/stark/duckapi/DuckapiApplication.java
@@ -0,0 +1,13 @@
+package com.stark.duckapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuckapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuckapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stark/duckapi/DuckapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stark.duckapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuckapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
